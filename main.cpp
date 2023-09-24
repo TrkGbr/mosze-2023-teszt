8fe6bb19316c8162b524371497852d4fa569037e
TrkGbr <torokg2002@gmail.com>
2023-09-24 16:35:12 +0200

    kijavítottam a említett hibákat

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS helyett a helyes: N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' // pontosvessző hiányzik a sor végén és idézőjel kell
-    for (int i = 0; ) //hiányzik a felső limit, illetve a lépésmérték
+    int *b = new int[N_ELEMENTS]; //NELEMENTS helyett a helyes: N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'; // pontosvessző hiányzik a sor végén és idézőjel kell
+    for (int i = 1; i<=N_ELEMENTS; i++) //hiányzik a felső limit, illetve a lépésmérték
     {
-        b[i] = i * 2; //i-1. tömbelemtől kéne számolni
+        b[i-1] = i * 2; //i-1. tömbelemtől kéne számolni
     }
     for (int i = 0; i; i++) //rossz az intervallum megadás
     {
-        std::cout << "Ertek:" //érték kiiratás és pontosvessző a sorvégén hiányzik
+        std::cout << "Ertek:" << b[i] << " "; //érték kiiratás és pontosvessző a sorvégén hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) //vessző helyett pontosvessző kell
+    for (int i = 0; i < N_ELEMENTS; i++) //vessző helyett pontosvessző kell
     {
-        atlag += b[i] //pontosvessző hiányzik
+        atlag += b[i]; //pontosvessző hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
