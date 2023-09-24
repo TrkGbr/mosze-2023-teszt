a35a5b08222ee5071017b9bc568cb49cec43279b
TrkGbr <torokg2002@gmail.com>
2023-09-24 16:19:23 +0200

    kikommenteltem a hibákat

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //NELEMENTS helyett a helyes: N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' // pontosvessző hiányzik a sor végén és idézőjel kell
+    for (int i = 0;) //hiányzik a felső limit, illetve a lépésmérték
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //i+1. tömbelemtől kéne számolni
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //rossz az intervallum megadás
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //érték kiiratás és pontosvessző a sorvégén hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) //vessző helyett pontosvessző kell
     {
-        atlag += b[i]
+        atlag += b[i] //pontosvessző hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
